03a584ae0e30f4760675129a52b75b6cfebf865b
seungdangdang <yseungun.21@gmail.com>
2025-05-07 17:34:32 +0900

    feat: Job 관련 RESTful API 구현 (POST, GET, 검색)

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Body,
+  Query,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { JobsService } from './jobs.service';
+import { CreateJobDto } from './dto/create-job.dto';
+import { Job } from './entities/job.entity';
 
 @Controller('jobs')
-export class JobsController {}
+export class JobsController {
+  constructor(private readonly jobsService: JobsService) {}
+
+  /**
+   * 새로운 작업을 생성한다.
+   */
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createJobDto: CreateJobDto): Job {
+    return this.jobsService.create(createJobDto);
+  }
+
+  /**
+   * 모든 작업 목록을 조회한다.
+   */
+  @Get()
+  async findAll(): Promise<Job[]> {
+    return await this.jobsService.findAll();
+  }
+
+  /**
+   * status / title 으로 작업을 검색한다.
+   */
+  @Get('search')
+  async search(
+    @Query('title') title?: string,
+    @Query('status') status?: string,
+  ): Promise<Job[]> {
+    return await this.jobsService.search(title, status);
+  }
+
+  /**
+   * 특정 작업의 상세 정보를 조회한다.
+   */
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Job> {
+    return await this.jobsService.findOne(id);
+  }
+}
